b7827bb50e2c0534d9e038ce298b9664f02cf9dc
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2024-08-12 12:39:22 +0530

    chore: Add socket.io client-side script for real-time communication

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 //set static folders
 app.use(express.static(path.join(__dirname, "public")));
 
+//Excetute when client connects
+io.on("connection", (socekt) => {
+  console.log("New client connection");
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
